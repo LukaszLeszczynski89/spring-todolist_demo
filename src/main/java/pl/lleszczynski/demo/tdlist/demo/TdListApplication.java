10822460bab5bef29ecf0d4a3568a67f0f617103
LukaszLeszczynski <lukaszleszczynski2@gmail.com>
2020-08-16 08:02:00 +0100

    first commit

---
+++ src/main/java/pl/lleszczynski/demo/tdlist/demo/TdListApplication.java
@@ -0,0 +1,13 @@
+package pl.lleszczynski.demo.tdlist.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (TdListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lleszczynski/demo/tdlist/demo/TdListApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lleszczynski.demo.tdlist.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
